0c2c019257941d3bdcb9bae79e73b94309dc4445
Bogushevich <mr.raccon321@gmail.com>
2023-09-28 09:43:23 +0900

    First try

---
+++ massive.cpp
@@ -0,0 +1,26 @@
+#include <cmath> //математические функции
+#include <stdlib.h> //вставляем код для случайных чисел
+#include <time.h>
+
+
+int sum_mass (int n; int* mas;){
+
+int sum;
+
+	for (unsigned i = 0; i < n; i++)
+		
+	 sum = pow(a[i],2) + sum;
+
+return sum;
+
+}
+
+void zap_mas(int *a; int n; int max; int min;){
+
+	srand(time(NULL));
+
+	for (unsigned i = 0; i < n+; i++)
+
+		a[i] = min + rand() % (max + min + 1)
+
+}
